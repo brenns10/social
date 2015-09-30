8e13a10af23991bbd8aa6e59b6890fa729fe698f
Stephen Brennan <smb196@case.edu>
2015-09-30 15:07:34 -0400

    Add logging for DemographicInfo, and ignore empty strings.

--- social/info.py
+++ social/info.py
@@ -3,10 +3,18 @@ Contains DemographicInfo class, which stores info found by accounts.
 """
 
 from collections import UserDict
+import logging
+log = logging.getLogger('social.info')
 
 class DemographicInfo(UserDict):
 
     def __setitem__(self, key, value):
+        # Ignore empty strings...
+        if type(value) is str and value == '':
+            return
+
+        log.debug('DemographicInfo: %r=%r', key, value)
+        # Otherwise, do the set stuff.
         if key in self.data:
             self.data[key].add(value)
         else:
